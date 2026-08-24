3f7fd720ccef12d65f9fc5c8bef60d40458e1140
Prashant SIngh <prashant247740@gmail.com>
2026-08-24 17:35:20 +0530

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     int levels(TreeNode* root){
+//         if(root == NULL) return 0;
+//         return 1 + max(levels(root->left), levels(root->right));
+//     }
+//     void nthLevel(TreeNode* root, int curr, int level, vector<int>& v){
+//         if(root == NULL) return;
+//         if(curr == level){
+//             v.push_back(root->val);
+//             return;
+//         }
+//         nthLevel(root->left, curr+1, level, v);
+//         nthLevel(root->right, curr+1, level, v);
+//     }
+//     void lOrder(TreeNode* root, vector<vector<int>>& ans){
+//         int n = levels(root);
+//         for(int i = 1; i <= n; i++){
+//             vector<int> v;
+//             nthLevel(root, 1, i, v);
+//             ans.push_back(v);
+//         }
+//     }
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> ans;
+//         lOrder(root, ans);
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+class Solution {
+public:
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        //(Using BFS)
+        vector<vector<int>> ans;
+        if(root == NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> level;
+
+            for(int i = 0; i < n; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                level.push_back(temp->val);
+
+                if(temp->left != NULL) q.push(temp->left);
+                if(temp->right != NULL) q.push(temp->right);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+
+    }
+};
