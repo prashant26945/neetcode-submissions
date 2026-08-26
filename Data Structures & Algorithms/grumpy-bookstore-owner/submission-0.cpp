74b4e40a074e018f21d0921c82cbb5b603f6bff4
Prashant SIngh <prashant247740@gmail.com>
2026-08-26 21:36:51 +0530

    Add: grumpy-bookstore-owner - submission-0

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        vector<int>& arr = customers;
+        int n = arr.size();
+        int k = minutes;
+        int prevLoss = 0;
+        for(int i = 0; i < k; i++){
+            if(grumpy[i] == 1) prevLoss += arr[i];
+        }
+        int maxLoss = prevLoss;
+        int maxIdx = 0;
+        
+        // Sliding window
+
+        int i = 1;
+        int j = k;
+        while(j < n){
+            int currLoss = prevLoss;
+            if(grumpy[j] == 1) currLoss += arr[j];
+            if(grumpy[i-1] == 1) currLoss -= arr[i-1];
+            if(maxLoss < currLoss){
+                maxLoss = currLoss;
+                maxIdx = i;
+            }
+            prevLoss = currLoss;
+            i++;
+            j++;
+        }
+
+        // Filling 0s in the grumpy array window
+
+        for(int i = maxIdx; i < (maxIdx+k); i++){
+            grumpy[i] = 0;
+        }
+
+        // Sum of satisfaction
+
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            if(grumpy[i] == 0) sum += arr[i];
+        }
+    
+    return sum;
+    }
+};
